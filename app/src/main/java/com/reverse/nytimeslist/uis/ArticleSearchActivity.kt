f83fc1ab25aed4d74b39258a6c23221a6c6fd2b8
russel febrishky <rfgalahad23@gmail.com>
2022-01-31 00:35:43 +0800

    Added loading progress bar for api calls

--- app/src/main/java/com/reverse/nytimeslist/uis/ArticleSearchActivity.kt
+++ app/src/main/java/com/reverse/nytimeslist/uis/ArticleSearchActivity.kt
@@ -1,8 +1,13 @@
 package com.reverse.nytimeslist.uis
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.reverse.nytimeslist.AppUtil
@@ -21,6 +26,7 @@ import retrofit2.Response
 class ArticleSearchActivity: AppCompatActivity(), View.OnClickListener {
 
     private var apiClient: ApiServices? = null
+    var pd: Dialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,9 +37,28 @@ class ArticleSearchActivity: AppCompatActivity(), View.OnClickListener {
         tv_search_button.setOnClickListener(this)
     }
 
+    fun showProgressDialog() {
+        pd?.dismiss()
+        pd = Dialog(this)
+        pd?.setContentView(R.layout.dialog_custom_progress)
+        pd?.setCancelable(false)
+        val window = pd?.window
+        if (window != null) {
+            window.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
+            window.setGravity(Gravity.CENTER)
+            window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        }
+        pd?.show()
+    }
+
+    fun hideProgressDialog() {
+        pd?.dismiss()
+    }
+
     override fun onClick(v: View?) {
         when (v) {
             tv_search_button -> {
+                showProgressDialog()
                 val query = et_search_query.text.toString()
                 val call = apiClient?.searchArticles(query, ApiConstants.apiKey)
 
@@ -42,6 +67,7 @@ class ArticleSearchActivity: AppCompatActivity(), View.OnClickListener {
                         call: Call<ArticleSearchResponse>,
                         response: Response<ArticleSearchResponse>
                     ) {
+                        hideProgressDialog()
                         val articles = response.body()!!.response!!.docs!!
                         val list = ArrayList<ListModel>()
                         if (!articles.isNullOrEmpty()) {
@@ -61,6 +87,7 @@ class ArticleSearchActivity: AppCompatActivity(), View.OnClickListener {
                     }
 
                     override fun onFailure(call: Call<ArticleSearchResponse>, t: Throwable) {
+                        hideProgressDialog()
                         Toast.makeText(this@ArticleSearchActivity, t.toString(), Toast.LENGTH_SHORT).show()
                     }
                 })
--- app/src/main/java/com/reverse/nytimeslist/uis/MainActivity.kt
+++ app/src/main/java/com/reverse/nytimeslist/uis/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.reverse.nytimeslist.uis
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import com.reverse.nytimeslist.R
 import com.reverse.nytimeslist.api.ApiConstants
@@ -20,6 +25,7 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private var apiClient: ApiServices? = null
+    var pd: Dialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,18 +39,38 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         tv_most_viewed.setOnClickListener(this)
     }
 
+    fun showProgressDialog() {
+        pd?.dismiss()
+        pd = Dialog(this)
+        pd?.setContentView(R.layout.dialog_custom_progress)
+        pd?.setCancelable(false)
+        val window = pd?.window
+        if (window != null) {
+            window.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
+            window.setGravity(Gravity.CENTER)
+            window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        }
+        pd?.show()
+    }
+
+    fun hideProgressDialog() {
+        pd?.dismiss()
+    }
+
     override fun onClick(v: View?) {
         when (v) {
             tv_search_articles -> {
                 startActivity(Intent(this, ArticleSearchActivity::class.java))
             }
             tv_most_emailed -> {
+                showProgressDialog()
                 val call = apiClient?.mostEmailed(ApiConstants.apiKey)
                 call?.enqueue(object : Callback<MostPopularResponse> {
                     override fun onResponse(
                         call: Call<MostPopularResponse>,
                         response: Response<MostPopularResponse>
                     ) {
+                        hideProgressDialog()
                         val articles = response.body()!!.results!!
                         val list = ArrayList<ListModel>()
                         if (!articles.isNullOrEmpty()) {
@@ -64,17 +90,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     }
 
                     override fun onFailure(call: Call<MostPopularResponse>, t: Throwable) {
+                        hideProgressDialog()
                         Toast.makeText(this@MainActivity, t.toString(), Toast.LENGTH_SHORT).show()
                     }
                 })
             }
             tv_most_shared -> {
+                showProgressDialog()
                 val call = apiClient?.mostShared(ApiConstants.apiKey)
                 call?.enqueue(object : Callback<MostPopularResponse> {
                     override fun onResponse(
                         call: Call<MostPopularResponse>,
                         response: Response<MostPopularResponse>
                     ) {
+                        hideProgressDialog()
                         val articles = response.body()!!.results!!
                         val list = ArrayList<ListModel>()
                         if (!articles.isNullOrEmpty()) {
@@ -94,17 +123,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     }
 
                     override fun onFailure(call: Call<MostPopularResponse>, t: Throwable) {
+                        hideProgressDialog()
                         Toast.makeText(this@MainActivity, t.toString(), Toast.LENGTH_SHORT).show()
                     }
                 })
             }
             tv_most_viewed -> {
+                showProgressDialog()
                 val call = apiClient?.mostViewed(ApiConstants.apiKey)
                 call?.enqueue(object : Callback<MostPopularResponse> {
                     override fun onResponse(
                         call: Call<MostPopularResponse>,
                         response: Response<MostPopularResponse>
                     ) {
+                        hideProgressDialog()
                         val articles = response.body()!!.results!!
                         val list = ArrayList<ListModel>()
                         if (!articles.isNullOrEmpty()) {
@@ -124,6 +156,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     }
 
                     override fun onFailure(call: Call<MostPopularResponse>, t: Throwable) {
+                        hideProgressDialog()
                         Toast.makeText(this@MainActivity, t.toString(), Toast.LENGTH_SHORT).show()
                     }
                 })
